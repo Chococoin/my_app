b7c0872fbac45fbf7b0414d03b4b556c126843c6
German Lugo <german.lugo.bitmibs.com>
2017-07-16 00:49:44 +0200

    Fix endpoint for show balance.

--- app.py
+++ app.py
@@ -1,29 +1,34 @@
 from flask import Flask, render_template, make_response
 from flask_restful import Resource, Api
 import datetime
+import krakenex
+
 
 app = Flask(__name__)
 api = Api(app)
 
 class Index(Resource):
     def get(self):
-    	header = {'Content-Type': 'text/html'}
-        return make_response(render_template('index.html'),200, header)
+        k = krakenex.API()
+        k.load_key('kraken.key')
+        balance = k.query_private('Balance')
+        header = {'Balance': balance}
+        return header
 
 class Kraken_api(Resource):
-	def get(self):
-		return {'resource': 'Balance'}
+    def get(self):
+        return {'resource': 'Balance'}
 
 class Hello(Resource):
-	def get(self):
-		header = {'Content-Type': 'text/html'}
-		now = datetime.datetime.now()
-		timeString = now.strftime("%d-%m-%Y %H:%M:%S")
-		templateData = {
-			'title' : 'HELLO TIME!',
-			'time': timeString
-			} 
-		return make_response(render_template('hello_time.html', **templateData), 200, header)
+    def get(self):
+        header = {'Content-Type': 'text/html'}
+        now = datetime.datetime.now()
+        timeString = now.strftime("%d-%m-%Y %H:%M:%S")
+        templateData = {
+            'title' : 'HELLO TIME!',
+            'time': timeString
+            } 
+        return make_response(render_template('hello_time.html', **templateData), 200, header)
 
 api.add_resource(Index, '/')
 api.add_resource(Kraken_api, '/api')
